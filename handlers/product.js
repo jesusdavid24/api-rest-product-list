96c81d2a157dce3bfb6fd70b93f9cb18f153dcbb
jesus bravo <jd_bravo24@hotmail.com>
2023-07-09 11:49:59 -0500

    edit msg

--- handlers/product.js
+++ handlers/product.js
@@ -1,71 +1,77 @@
 const {
-   createProduct,
-   getProduct,
-   getProductById,
-   updateProduct,
-   deleteProduct,
+    createProduct,
+    getProduct,
+    getProductById,
+    updateProduct,
+    deleteProduct,
 } = require("../models/product");
 
 exports.healthCheck = (req, res) => {
-   res.status(200).send("Api runing");
+    res.status(200).send("Api runing");
 };
 
 exports.handleGetAllProducts = (req, res) => {
-   try {
-      const data = getProduct();
-      res.status(201).json(data);
-   } catch (error) {
-      res.status(400).json({ message: error.message });
-   }
+    try {
+        const data = getProduct();
+
+        res.status(201).json(data);
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 };
 
 exports.handleGetProductsById = (req, res) => {
    try {
-      const { id } = req.params;
-      const data = getProductById(id);
-      res.status(201).json(data);
+        const { id } = req.params;
+        const data = getProductById(id);
+
+        res.status(201).json(data);
+
    } catch (error) {
-      res.status(400).json({ message: error.message });
-   }
+        res.status(400).json({ message: error.message });
+   }    
 };
 
 exports.handlePostProducts = (req, res) => {
-   try {
-      const { product_name, color, category, price } = req.body;
+    try {
+        const { product_name, color, category, price } = req.body;
 
-      if (!(product_name && color && category && price)) {
-         return res.status(400).json({ message: "Some fields are missing to fill out" });
-      }
+        if (!(product_name && color && category && price)) {
+            return res.status(400).json({ message: "Some fields are missing to fill out" });
+        }
 
-      const data = createProduct({ product_name, color, category, price });
+        const data = createProduct({ product_name, color, category, price });
 
-      res.status(201).json(data);
-   } catch (error) {
-      res.status(400).json({ message: error.message });
-   }
+        res.status(201).json(data);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 };
 
 exports.handleUpdateProduct = (req, res) => {
-   try {
-      const { id } = req.params;
-      const { product_name, color, category, price } = req.body;
+    try {
+        const { id } = req.params;
+        const { product_name, color, category, price } = req.body;
 
-      const data = updateProduct(id, { product_name, color, category, price });
+        const data = updateProduct(id, { product_name, color, category, price });
 
-      res.status(200).json(data);
-   } catch (error) {
-      res.status(400).json({ message: error.message });
-   }
+        res.status(200).json(data);
+
+    } catch (error) {
+    res.status(400).json({ message: error.message });
+    }
 };
 
 exports.handleDeleteProduct = (req, res) => {
-   try {
-      const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-      const data = deleteProduct(id);
+        const data = deleteProduct(id);
 
-      res.status(201).json({ data });
-   } catch (error) {
-      res.status(400).json({ message: error.message });
-   }
+        res.status(201).json({ data });
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 };
